8f3becdbc98529b95c7b427f4fa208b882d546e3
XpycTee <i@xpyctee.ru>
2023-10-04 14:32:54 +0500

    Update version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ keywords = ("asterisk", "manager", "interface",
 
 setup(
     name='ami-client',
-    version='1.1.0rc',
+    version='1.1.2rc',
     packages=['ami', 'ami.client'],
     url='https://github.com/XpycTee/ami-client',
     license='Apache-2.0 license',
